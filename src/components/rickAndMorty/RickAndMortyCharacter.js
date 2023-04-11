fce7f2a5a9b4bc3d1014d85c779751854256ede9
Orestarxx <oresto322@gmail.com>
2023-04-11 17:16:56 +0300

    in progress

--- src/components/rickAndMorty/RickAndMortyCharacter.js
+++ src/components/rickAndMorty/RickAndMortyCharacter.js
@@ -1,9 +1,14 @@
 import React from 'react';
 
-const RickAndMortyCharacter = () => {
+const RickAndMortyCharacter = ({character}) => {
+    let {name,status,species,gender,image} = character;
     return (
         <div>
-            
+            <div>{name}</div>
+            <div>{status}</div>
+            <div>{species}</div>
+            <div>{gender}</div>
+            <img src={image} alt=""/>
         </div>
     );
 };
--- src/components/rickAndMorty/RickAndMortyCharacters.js
+++ src/components/rickAndMorty/RickAndMortyCharacters.js
@@ -1,18 +1,14 @@
-import React, {useEffect, useState} from 'react';
+import React from 'react';
+import {characters} from "../../data/rickAndMortyCharacters.data";
+import RickAndMortyCharacter from "./RickAndMortyCharacter";
+
 
 
 const RickAndMortyCharacters = () => {
-    const {characters,setCharacters} = useState()
-    useEffect(() =>{
-    fetch('https://rickandmortyapi.com/api/character')
-        .then(value => value.json())
-        .then(characters =>console.log())
 
-    },[]);
-    console.log(characters);
     return (
         <div>
-            {}
+            {characters.map(character =><RickAndMortyCharacter character={character} key={character.id}/>)}
         </div>
     );
 };
