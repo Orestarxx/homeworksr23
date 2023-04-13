2dffc84c44e55cc38e83901ff48a36996cfaa4f1
Orestarxx <oresto322@gmail.com>
2023-04-13 03:18:09 +0300

    in progress

--- src/components/posts/PostBuild.js
+++ src/components/posts/PostBuild.js
@@ -1,9 +1,16 @@
 import React from 'react';
 
-const PostBuild = () => {
+
+const PostBuild = ({post,lift,postsBody}) => {
+    let {id,title,userId,body} = post;
     return (
         <div>
-            
+            <div>{id}</div>
+            <div>{title}</div>
+            <button onClick={() =>{
+                lift(post)
+            }}>click</button>
+
         </div>
     );
 };
--- src/components/posts/Posts.js
+++ src/components/posts/Posts.js
@@ -4,12 +4,18 @@ import PostBuild from "./PostBuild";
 
 const Posts = () => {
     let [posts,setPosts] = useState([]);
+    let [postsBody,setPostsBody] = useState(null);
+    const lift = (obj) =>{
+        setPostsBody({...obj})
+    }
+    console.log(postsBody);
     useEffect(() =>{
         getPosts().then(value => setPosts(value.data))
     },[])
     return (
         <div>
-            posts.map(post =><PostBuild/>)
+            {posts.map(post => <PostBuild post={post} key={post.id} lift={lift} postsBody={postsBody}/>)}
+            {postsBody && <div>{postsBody.id}</div>}
         </div>
     );
 };
